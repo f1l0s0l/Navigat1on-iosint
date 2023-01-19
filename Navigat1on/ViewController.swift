0c6ca5f8bde11feaa9fb1b891fc74136b87d4991
Sidorik Ilia <iss19.spb@gmail.com>
2023-01-19 23:34:34 +0700

    Initial Commit

---
+++ Navigat1on/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navigat1on
+//
+//  Created by Илья Сидорик on 19.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
